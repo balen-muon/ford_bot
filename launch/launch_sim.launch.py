e72c53db3638c1c9e2161d594ad8a7d6a94f57ef
balen-muon <balen@muontech.co.uk>
2024-02-07 11:30:43 +0000

    Add all new and modified files to ws.

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -18,23 +18,31 @@ def generate_launch_description():
     rsp = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory(package_name),'launch','rsp.launch.py'
-                )]), launch_arguments={'use_sim_time': 'true'}.items()
+                )]), launch_arguments={'use_sim_time': 'true',
+                                       'world':'./src/ford_bot/worlds/obstacles.world'}.items()
     )
 
         # Include the Gazebo launch file, provided by the gazebo_ros package
     gazebo = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
-    )
+                    get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]))
 
     spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
                         arguments=['-topic', 'robot_description',
                                    '-entity', 'ford_bot'],
                         output='screen')
     
+    navigation = IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([os.path.join(
+                get_package_share_directory(package_name),'launch','navigation_launch.launch.py'
+            )]), launch_arguments={'use_sim_time': 'true',
+                                    'map': './src/robot/maps/map_save.yaml'}.items()
+)
+    
         # Launch them all!
     return LaunchDescription([
         rsp,
         gazebo,
         spawn_entity,
+        # navigation,
     ])
